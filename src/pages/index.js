bb2486a29ba40f586082d8e30bfad5daed071eb4
colbyfayock <fayock@gmail.com>
2022-08-11 10:38:25 -0400

    Bonus: fixing lint issue with useRef hook abstracting into custom component

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,6 +18,40 @@ const cld = new Cloudinary({
   }
 });
 
+const VideoPreview = ({ video }) => {
+  const playerRef = useRef();
+
+  function onMouseOver() {
+    playerRef.current.videoRef.current.play();
+  }
+
+  function onMouseOut() {
+    playerRef.current.videoRef.current.pause();
+  }
+
+  return (
+    <div className={styles.video} onMouseOver={onMouseOver} onMouseOut={onMouseOut}>
+      <AdvancedImage
+        cldImg={cld.image(video.id).setAssetType('video').delivery('q_auto').format('auto:image')}
+      />
+      <AdvancedVideo
+        ref={playerRef}
+        loop
+        muted
+        width="100%"
+        cldVid={cld.video(video.id).effect('e_preview:duration_4').delivery('q_auto').format('auto')}
+        plugins={[lazyload()]}
+      />
+
+      <h3 className={styles.videoTitle}>
+        <a href={video.link} rel="noreferrer">
+          { video.title }
+          </a>
+      </h3>
+    </div>
+  )
+}
+
 export default function Home() {
   return (
     <Layout>
@@ -33,35 +67,9 @@ export default function Home() {
 
         <ul className={styles.videos}>
           {videos.map(video => {
-            const playerRef = useRef();
-
-            function onMouseOver() {
-              playerRef.current.videoRef.current.play();
-            }
-
-            function onMouseOut() {
-              playerRef.current.videoRef.current.pause();
-            }
-
             return (
-              <li className={styles.video} key={video.id} onMouseOver={onMouseOver} onMouseOut={onMouseOut}>
-                <AdvancedImage
-                  cldImg={cld.image(video.id).setAssetType('video').delivery('q_auto').format('auto:image')}
-                />
-                <AdvancedVideo
-                  ref={playerRef}
-                  loop
-                  muted
-                  width="100%"
-                  cldVid={cld.video(video.id).effect('e_preview:duration_4').delivery('q_auto').format('auto')}
-                  plugins={[lazyload()]}
-                />
-
-                <h3 className={styles.videoTitle}>
-                  <a href={video.link} rel="noreferrer">
-                    { video.title }
-                    </a>
-                </h3>
+              <li key={video.id}>
+                <VideoPreview video={video} />
               </li>
             )
           })}
